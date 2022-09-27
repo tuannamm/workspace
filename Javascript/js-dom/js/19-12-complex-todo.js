b2e0e2d18c9aeba210f6fc2859d932aa033eb974
Tuan Nam <nttnnam@gmail.com>
2022-09-27 22:28:24 +0700

    learn DOM

---
+++ Javascript/js-dom/js/19-12-complex-todo.js
@@ -0,0 +1,35 @@
+function createTodoElement(todo) {
+  if (!todo) return null;
+
+  const liElement = document.createElement('li');
+  liElement.textContent = todo.title;
+  liElement.dataset.id = todo.id;
+
+  return liElement;
+}
+
+function renderTodoList(todoList, ulElementId) {
+  if (!Array.isArray(todoList) || todoList.length === 0) return;
+
+  // find ulElement
+  // loop through todoList
+  // each todo --> create li element --> append to ul
+  const ulElement = document.getElementById(ulElementId);
+
+  if (ulElement) {
+    for (let i = 0; i < todoList.length; i++) {
+      const liElement = createTodoElement(todoList[i]);
+      ulElement.appendChild(liElement);
+    }
+  }
+}
+
+// main
+(() => {
+  const todoList = [
+    { id: 1, title: 'Learn Javascript' },
+    { id: 2, title: 'Learn ReactJS' },
+    { id: 3, title: 'Learn NextJS' },
+  ];
+  renderTodoList(todoList, 'todoList');
+})();
