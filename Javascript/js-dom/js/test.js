01602d0c9a4c8b430b30448617c78bfa768993db
Tuan Nam <nttnnam@gmail.com>
2022-09-28 11:38:06 +0700

    fix shortcut keyboard

---
+++ Javascript/js-dom/js/test.js
@@ -0,0 +1,12 @@
+const test = (expr) => {
+  switch (expr) {
+    case 'Oranges':
+    case 'Mangoes':
+    case 'Papayas':
+      console.log(expr);
+      // expected output: "Mangoes and papayas are $2.79 a pound."
+      break;
+    default:
+      console.log(`Sorry, we are out of ${expr}.`);
+  }
+};
