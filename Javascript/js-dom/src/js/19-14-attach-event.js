1ce014eb65e9625947b7f4643173cfbabdd5fe95
Tuan Nam <nttnnam@gmail.com>
2022-09-28 23:00:20 +0700

    fix shortcut keyboard

---
+++ Javascript/js-dom/src/js/19-14-attach-event.js
@@ -0,0 +1,39 @@
+const createTodoList = (todo) => {
+  if (!todo) return;
+
+  // find template
+  const todoTemplate = document.getElementById('todoTemplate');
+  if (!todoTemplate) return null;
+
+  // clone li element
+  const todoElement = todoTemplate.content.firstElementChild.cloneNode(true);
+  todoElement.dataset.id = todo.id;
+
+  // update content
+  const titleElement = todoElement.querySelector('.todo__title');
+  if (titleElement) titleElement.textContent = todo.title;
+
+  return todoElement;
+};
+
+const renderTodoList = (todoList, ulElement) => {
+  if (!Array.isArray(todoList) || todoList.length === 0) return null;
+
+  const ulListElement = document.getElementById('todoList');
+  if (!ulListElement);
+
+  for (let i = 0; i < todoList.length; i++) {
+    const liElement = createTodoList(todoList[i]);
+    ulListElement.appendChild(liElement);
+  }
+};
+
+// main
+(() => {
+  const todoList = [
+    { id: 1, title: 'Learn Javascript' },
+    { id: 2, title: 'Learn ReactJS' },
+    { id: 3, title: 'Learn NextJS' },
+  ];
+  renderTodoList(todoList, 'todoList');
+})();
