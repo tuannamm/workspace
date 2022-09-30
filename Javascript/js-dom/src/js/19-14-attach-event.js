e3d5be95727b18ee8cd0b7e69a1a2567ca61f8b0
Tuan Nam <nttnnam@gmail.com>
2022-09-30 15:19:54 +0700

    fix shortcut keyboard

--- Javascript/js-dom/src/js/19-14-attach-event.js
+++ Javascript/js-dom/src/js/19-14-attach-event.js
@@ -54,12 +54,12 @@ const getTodoList = () => {
 
 // main
 (() => {
-  // const todoList = [
-  //   { id: 1, title: 'Learn Javascript' },
-  //   { id: 2, title: 'Learn ReactJS' },
-  //   { id: 3, title: 'Learn NextJS' },
-  // ];
+  const todoList = [
+    { id: 1, title: 'Learn Javascript' },
+    { id: 2, title: 'Learn ReactJS' },
+    { id: 3, title: 'Learn NextJS' },
+  ];
 
-  const todoList = getTodoList();
+  // const todoList = getTodoList();
   renderTodoList(todoList, 'todoList');
 })();
