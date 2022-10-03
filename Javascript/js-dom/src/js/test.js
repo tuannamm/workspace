e2f5d7a82e49be0b752e6060571f281f0c08b0ee
Tuan Nam <nttnnam@gmail.com>
2022-10-03 17:19:06 +0700

    update

--- Javascript/js-dom/src/js/test.js
+++ Javascript/js-dom/src/js/test.js
@@ -17,11 +17,11 @@ const renderTodoList = (todoList, ulElement) => {
   if (!Array.isArray(todoList) || todoList.length === 0) return null;
 
   const ulListElement = document.getElementById(ulElement);
-  if (ulListElement) console.log(ulListElement);
+  if (!ulListElement) return;
 
   for (let i = 0; i < todoList.length; i++) {
-    const liListElement = createTodoList(todoList[i]);
-    ulListElement.appendChild(liListElement);
+    const liElement = createTodoList(todoList[i]);
+    ulListElement.appendChild(liElement);
   }
 };
 
