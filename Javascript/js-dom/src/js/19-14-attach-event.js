29aa432cdfb6bc6767862c47da4643f7ee472b9a
Tuan Nam <nttnnam@gmail.com>
2022-10-04 15:38:06 +0700

    update

--- Javascript/js-dom/src/js/19-14-attach-event.js
+++ Javascript/js-dom/src/js/19-14-attach-event.js
@@ -68,12 +68,12 @@ const getTodoList = () => {
 
 // main
 (() => {
-  const todoList = [
-    { id: 1, title: 'Learn Javascript', status: 'pending' },
-    { id: 2, title: 'Learn ReactJS', status: 'completed' },
-    { id: 3, title: 'Learn NextJS', status: 'pending' },
-  ];
+  // const todoList = [
+  //   { id: 1, title: 'Learn Javascript', status: 'pending' },
+  //   { id: 2, title: 'Learn ReactJS', status: 'completed' },
+  //   { id: 3, title: 'Learn NextJS', status: 'pending' },
+  // ];
 
-  // const todoList = getTodoList();
+  const todoList = getTodoList();
   renderTodoList(todoList, 'todoList');
 })();
