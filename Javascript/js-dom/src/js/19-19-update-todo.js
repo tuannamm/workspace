e8cb97382b5b7468a0a7b989e3280239d036dc80
Tuan Nam <nttnnam@gmail.com>
2022-10-06 16:24:23 +0700

    update

--- Javascript/js-dom/src/js/19-19-update-todo.js
+++ Javascript/js-dom/src/js/19-19-update-todo.js
@@ -137,12 +137,6 @@ const handleTodoFormSubmit = (e) => {
 
 // main
 (() => {
-  // const todoList = [
-  //   { id: 1, title: 'Learn Javascript', status: 'pending' },
-  //   { id: 2, title: 'Learn ReactJS', status: 'completed' },
-  //   { id: 3, title: 'Learn NextJS', status: 'pending' },
-  // ];
-
   const todoList = getTodoList();
 
   renderTodoList(todoList, 'todoList');
