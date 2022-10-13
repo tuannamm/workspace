ecced0b767382cc223d7b7c242d4451896f613ad
Tuan Nam <nttnnam@gmail.com>
2022-10-13 15:59:56 +0700

    bank-withdraw-deposit

---
+++ Javascript/js-dom/src/js/19-23-persist-filter.js
@@ -0,0 +1,65 @@
+const getAllTodoElements = () => {
+  return document.querySelectorAll('#todoList > li');
+};
+
+const isMatch = (liElement, searchTerm) => {
+  if (!liElement) return false;
+  if (searchTerm === '') return true;
+
+  // searchTerm === empty => show all todo
+  // searchTerm !== empty => filter todo
+
+  const titleElement = liElement.querySelector('p.todo__title');
+  if (!titleElement) return false;
+
+  const title = titleElement.textContent;
+  if (!title) return false;
+
+  return title.toLowerCase().includes(searchTerm.toLowerCase());
+};
+
+const searchTodo = (searchTerm) => {
+  const todoElementList = getAllTodoElements();
+
+  for (const todoElement of todoElementList) {
+    const needToShow = isMatch(todoElement, searchTerm);
+    todoElement.hidden = !needToShow;
+  }
+};
+
+const initSearchInput = () => {
+  // find search term input
+  const searchInput = document.getElementById('searchTerm');
+  if (!searchInput) return;
+
+  // attach change event
+  searchInput.addEventListener('input', () => {
+    searchTodo(searchInput.value);
+  });
+};
+
+const filterTodo = (filterStatus) => {
+  const todoElementList = getAllTodoElements();
+
+  for (const todoElement of todoElementList) {
+    const needToShow = filterStatus === 'all' || todoElement.dataset.status === filterStatus;
+    todoElement.hidden = !needToShow;
+  }
+};
+
+const initFilterStatus = () => {
+  // find select
+  const filterStatusSelect = document.getElementById('statusFilter');
+  if (!filterStatusSelect) return;
+
+  // attach change event
+  filterStatusSelect.addEventListener('change', () => {
+    filterTodo(filterStatusSelect.value);
+  });
+};
+
+// main
+(() => {
+  initSearchInput();
+  initFilterStatus();
+})();
